14ed56b158418ef045125d9f0962b4c7fdcfce59
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-10-23 22:17:39 +0500

    Added Crousal in Home Page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
         menuToggle.classList.toggle('active');
     });
 });
+
+// Crousal
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('.quote-slide');
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.remove('active');
+            if (i === index) {
+                slide.classList.add('active');
+            }
+        });
+    }
+
+    setInterval(function () {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        showSlide(currentIndex);
+    }, 3000); // Change every 3 seconds
+});
+
+
