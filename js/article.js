565b0bfe0490010b817efb2bd5fd65f94361d953
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-10-24 18:42:36 +0500

    Nhn ho raha zayada optimize

--- js/article.js
+++ js/article.js
@@ -65,3 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.getElementById('goBackButton').addEventListener('click', function() {
+    window.history.back();
+});
