94c3653dc40c0c4a08a88712809b2e3bf0f1147d
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-11-21 17:20:18 +0500

    Added Prisma ORM

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,33 @@
-const register = async(req,res) => {
+const prisma = require("../database/db.config");
+const bcrypt = require('bcrypt');
+
+const register = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (user) {
     return res
-      .status(200)
-      .json({ message: "Working Fine" });
-}
+      .status(400)
+      .json({ message: "A User Exists with same email and password" });
+  }
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const new_user = await prisma.user.create({
+    data: {
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    },
+  });
+
+  return res.status(200).json({ message: "User Created", new_user });
+};
 
 module.exports = {
-    register,
-}
+  register,
+};
