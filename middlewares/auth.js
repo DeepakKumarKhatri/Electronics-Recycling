d4756f083fb121d72adba25f728260607b94bf17
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-11-22 14:32:07 +0500

    Fixed Middlware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,21 @@
 const { getUser } = require("../service/auth");
+const path = require("path");
 
 async function verifyUserSession(req, res, next) {
   const user_session_id = req.cookies?.uid;
-  console.log("user_session_id: ",user_session_id);
+  console.log("user_session_id: ", user_session_id);
 
   if (!user_session_id) {
-    return res.redirect("public/index.html");
+    return res.sendFile(
+      path.join(__dirname, "../public/pages/auth/index.html")
+    );
   }
 
   const user = getUser(user_session_id);
   if (!user) {
-    return res.redirect("index");
+    return res.sendFile(
+      path.join(__dirname, "../public/pages/auth/index.html")
+    );
   }
   req.user = user;
   next();
